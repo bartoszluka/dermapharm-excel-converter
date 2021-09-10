85a5938ddc278b157ecd6270262d4f8067001b7f
bartoszluka <bartoszluka1@gmail.com>
2021-09-10 23:04:59 +0200

    Test new converter in console app

--- myFSharpApp/ConsoleApp.fs
+++ myFSharpApp/ConsoleApp.fs
@@ -2,8 +2,10 @@ open ExcelConverter
 
 [<EntryPoint>]
 let main args = 
-    convert 
-        "C:/Users/Bartek/source/repos/FSharp/ConverterApp/ExcelConverter/excel/Kopia document.xls"
-        "C:/Users/Bartek/source/repos/FSharp/ConverterApp/ExcelConverter/excel/CENNIK.xlsx"
-        |> printfn "%A"
+    // convert 
+    //     "C:/Users/Bartek/source/repos/FSharp/ConverterApp/ExcelConverter/excel/Kopia document.xls"
+    //     "C:/Users/Bartek/source/repos/FSharp/ConverterApp/ExcelConverter/excel/CENNIK.xlsx"
+    //     |> printfn "%A"
+    convertKakadu "C:/Users/Bartek/source/repos/FSharp/ConverterApp/ExcelConverter/excel/Dermapharm_Zam_2021-09-02_skl.xlsx" "C:/Users/Bartek/source/repos/FSharp/ConverterApp/ExcelConverter/excel/CENNIK.xlsx" 
+    |> printfn "%A"
     0
