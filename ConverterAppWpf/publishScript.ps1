112d20cd2768ff2440db25d167022f4600c8010c
bartoszluka <bartoszluka1@gmail.com>
2021-09-23 10:46:41 +0200

    Reformat publish

--- ConverterAppWpf/publishScript.ps1
+++ ConverterAppWpf/publishScript.ps1
@@ -1,5 +1,5 @@
-dotnet publish -c Release -f net5.0-windows -r win10-x64 --self-contained true -o Published -p:PublishSingleFile=true
-cp Publish.wxs Published
-cd Published
-candle Publish.wxs
+dotnet publish -c Release -f net5.0-windows -r win10-x64 --self-contained true -o Published -p:PublishSingleFile=true &&
+cp Publish.wxs Published &&
+cd Published &&
+candle Publish.wxs &&
 light Publish.wixobj
